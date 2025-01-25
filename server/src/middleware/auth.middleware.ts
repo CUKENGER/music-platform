84ba69fd119191f83ac80133aa6f685724dd4553
CUKENGER <andreychuvashov2015@gmail.com>
2025-01-25 03:43:14 +0300

    fix: Add skipped arg
    Add skipped arg `res` to middleware

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -1,40 +1,49 @@
-import { Injectable, NestMiddleware, UnauthorizedException } from '@nestjs/common';
-import { NextFunction, Request, Response } from 'express';
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { ApiError } from 'exceptions/api.error';
+import { Request } from 'express';
+import { NextFunction } from 'express-serve-static-core';
 import { TokenService } from 'models/token/token.service';
+import { Logger } from 'nestjs-pino';
 
 export interface RequestWithUser extends Request {
-  user?: any;
+  user?: unknown;
 }
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-  constructor(private tokenService: TokenService) {}
+  constructor(
+    private tokenService: TokenService,
+    private readonly logger: Logger,
+  ) { }
 
-  use(req: RequestWithUser, res: Response, next: NextFunction) {
-    try {
-      const authorizationHeader = req.headers.authorization;
+  use(req: RequestWithUser, _res: Response, next: NextFunction) {
+    this.logger.log(`Request to: ${req.method} ${req.path}`);
 
-      if (!authorizationHeader) {
-        throw new UnauthorizedException('Authorization header missing');
-      }
+    const authorizationHeader = req.headers.authorization;
 
-      const accessToken = authorizationHeader.split(' ')[1];
-      if (!accessToken) {
-        throw new UnauthorizedException('Access token missing');
-      }
-
-      const tokenData = this.tokenService.validateAccessToken(accessToken);
-      if (!tokenData) {
-        throw new UnauthorizedException('Invalid access token');
-      }
-
-      req.user = tokenData;
-
-      next();
-    } catch (error) {
-      console.error(`Error in AuthMiddleware: ${error.message}`);
-
-      next(new UnauthorizedException('User is not authorized'));
+    this.logger.log(`AuthMiddleware authorizationHeader: ${authorizationHeader}`);
+    if (!authorizationHeader) {
+      this.logger.error(`Authorization header missing`);
+      throw ApiError.UnauthorizedError('Authorization header missing');
     }
+
+    const accessToken = authorizationHeader.split(' ')[1];
+
+    this.logger.log(`AuthMiddleware accessToken: ${accessToken}`);
+    if (!accessToken) {
+      this.logger.error(`Access token missing`);
+      throw ApiError.UnauthorizedError('Access token missing');
+    }
+
+    const tokenData = this.tokenService.validateAccessToken(accessToken);
+    this.logger.log(`AuthMiddleware tokenData:`, { tokenData: tokenData });
+    if (!tokenData) {
+      this.logger.error(`Invalid access token`);
+      throw ApiError.UnauthorizedError('Invalid access token');
+    }
+
+    req.user = tokenData;
+
+    next();
   }
 }
