93ef87238485f3322af0ba46808a60a3ec36bb21
CUKENGER <andreychuvashov2015@gmail.com>
2025-06-17 12:06:16 +0300

    chore: update server configs
    - update eslint tsconfig prettier
    - remove unused components on client

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,13 +1,33 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   {
-    languageOptions: { globals: globals.browser },
+    languageOptions: {
+      globals: globals.node,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': ['error', {}, { usePrettier: true }],
+      'arrow-body-style': ['error', 'as-needed'],
+      'prefer-arrow-callback': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
 ];
